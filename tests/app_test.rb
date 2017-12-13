8b50f35b989ee0b17ae65dd778f5b6466721f4d5
Conor <office@conorryan.ie>
2017-12-13 12:14:00 +0000

    Added complex test simulation

--- tests/app_test.rb
+++ tests/app_test.rb
@@ -10,43 +10,59 @@ class MainAppTest < Minitest::Test
   def app
     Sinatra::Application
   end
-
+  #
   # GET /
+  #
   def test_api_root
     get '/'
     assert last_response.ok?
-    assert_equal 'Welcome to Robot Server API', last_response.body
+    assert_equal "Welcome to Robot Server API\n", last_response.body
   end
 
-  # GET /robots
+  #
+  # GET /server/health
+  #
   def test_get_health
     get '/server/health'
     assert last_response.ok?
-    assert_equal 'Healthy', last_response.body
+    assert_equal "Healthy\n", last_response.body
   end
   
+  #
   # GET /robots
+  #
   def test_get_robots
     get '/robots'
     assert_equal 200, last_response.status
   end
 
-  # GET /review/:id
-  # def test_get_robot_by_id
-  #   #get 'review' , params: {id: 1}
-  #   get "/robot/1" #, :id => 1
-  #   assert_equal 200, last_response.status
-  #   assert_includes last_response.content_type, 'application/json'
-  #   assert_includes last_response.body, 'Robbie'
-
-  #   # assert last_response.ok?
-  #   # response = JSON.parse(last_response.body)
-  #   # assert_equal 5, response.count
-  # end
-
-  # DELETE /review/:id
+  # DELETE /robot/:id
   # def test_delete_robot
   #   delete '/robot/1'
   #   assert_equal 200, last_response.status
   # end
+
+  #
+  # TESTS behaviousr
+  #
+  def test_simulation
+    get '/initialize'
+    assert_equal 200, last_response.status
+    
+    get '/robot/recharge/1'
+    assert_equal 200, last_response.status
+
+    get '/robot/start/1'
+    assert_equal 200, last_response.status
+
+    get '/robot/recharge/1'
+    assert_equal 400, last_response.status
+
+    get '/robot/stop/1'
+    assert_equal 200, last_response.status
+
+
+  end
+
+
 end
